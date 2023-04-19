f35faaacd2e4c104e71d80a13617f06b56f124ca
winterfellll <93076372+winterfellll@users.noreply.github.com>
2023-04-19 17:42:21 +0800

    Delete httprunner_demo directory

--- httprunner_demo/testcases/main.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pytest
-
-if __name__ == '__main__':
-    # pytest.main(['demo_testcase_request_test.py', '-s', '-q', '--clean-alluredir', '--alluredir=allure-results'])
-    # os.system(r"allure generate -c -o Report")
-    os.system('hrun demo_testcase_request.yml  --alluredir allure_results --clean-alluredir')
-    os.system('allure generate --clean allure_results -o Report/')
