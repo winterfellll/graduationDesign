e1be8fc9d934690eb6b6ef95f8e2dab02360dbc0
winterfellll <2312750867@qq.com>
2023-04-19 19:36:13 +0800

    fix

--- _Work/main.py
+++ _Work/main.py
@@ -1,5 +1,9 @@
 import os
 import pytest
+import sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 if __name__ == '__main__':
     pytest.main(['TestCase/', '-s', '-q', '--clean-alluredir', '--alluredir=./allure-results'])
